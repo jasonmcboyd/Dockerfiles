1856e09a5c202485e84c9f0111e0f077682d85ff
Jason Boyd <jason.boyd.ce@gmail.com>
2021-06-26 10:50:59 -0700

    Initial commit

---
+++ Dockerfiles/antlr/alpine/antlr-aliases.sh
@@ -0,0 +1,2 @@
+alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-$ANTLR_VERSION-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
+alias grun='java -Xmx500M -cp "/usr/local/lib/antlr-$ANTLR_VERSION-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
